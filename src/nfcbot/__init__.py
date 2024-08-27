b1331e6c9e9f599260e3f2ecf4a2aaa247bea1ff
wcam-wk <wcam00711@outlook.com>
2024-08-27 10:17:54 -0400

    Update __init__.py

--- src/nfcbot/__init__.py
+++ src/nfcbot/__init__.py
@@ -3,9 +3,9 @@
 from __future__ import annotations
 
 
-FILE_TPL_CAT = "File namespace templates"
-NFUR_TPL_CAT = "Non-free use rationale templates"
-NONFREE_FILE_CAT = "All non-free media"
+FILE_TPL_CAT = "檔案頁模板"
+NFUR_TPL_CAT = "非自由檔案文件依據模板"
+NONFREE_FILE_CAT = "所有非自由媒體"
 
 NONFREE_NO_REDUCE_TPL = ["non-free no reduce"]
-NONFREE_REDUCE_TPL = ["non-free reduce", "non-free manual reduce"]
+NONFREE_REDUCE_TPL = ["non-free reduce"]
